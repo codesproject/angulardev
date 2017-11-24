289f67afd88e033bfd9c672841bd567a14b70458
Jide Igun <ig.jide@gmail.com>
2017-11-24 15:31:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
 
-foodItem = ['Rice', 'Beans', 'Yam', 'Bread', 'Amala'];
-
-fav = this.foodItem[4];
-dispValue:string = '';
-showItem:boolean = false;
-itemCount: number;
-btnText: string = 'Add an Item';
-goalText: string = 'My first life goal';
-goals = [];
-
-constructor () { }
-
-ngOnInit() {
-  this.itemCount = this.goals.length;
-}
-
-addedList () {
-this.goals.push(this.goalText);
-this.goalText = '';
-this.itemCount = this.goals.length;
-
-  this.showItem = true;
-  this.btnText = "Item Added";
-}
-
-removeItem (i) {
-this.goals.splice(i, 1);
-}
-
-
+  food = ['Rice', 'Beans', 'Bread', 'Amala', 'Eba'];
+  fav = this.food[0];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,6 +29,4 @@ export class HomeComponent implements OnInit {
     removeItem (i) {
     this.goals.splice(i, 1);
     }
-
-
 }
